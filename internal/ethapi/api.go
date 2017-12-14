5c60e1ac2178fe5164e414e83a70a0cf9354babc
Andrey Samokhvalov <andrew.shvv@gmail.com>
2017-12-14 16:40:39 +0300

    ethapi: change pending transaction endpoint to return only incomin txs

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1214,15 +1214,6 @@ func (s *PublicTransactionPoolAPI) PendingTransactions() ([]*RPCTransaction, err
 
 	transactions := make([]*RPCTransaction, 0, len(pending))
 	for _, tx := range pending {
-		var signer types.Signer = types.HomesteadSigner{}
-		if tx.Protected() {
-			signer = types.NewEIP155Signer(tx.ChainId())
-		}
-		from, _ := types.Sender(signer, tx)
-		if _, err := s.b.AccountManager().Find(accounts.Account{Address: from}); err == nil {
-			transactions = append(transactions, newRPCPendingTransaction(tx))
-		}
-
 		if _, err := s.b.AccountManager().Find(accounts.Account{Address: *tx.To()}); err == nil {
 			transactions = append(transactions, newRPCPendingTransaction(tx))
 		}
